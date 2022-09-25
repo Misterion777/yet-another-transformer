4d1389163feaa08159646910c1ea3217f7b327e5
Ilya Lasy <ilyalas@gmail.com>
2022-09-25 19:46:52 +0300

    fixed layers, added self mask, added generator model

--- src/modules.py
+++ src/modules.py
@@ -6,6 +6,7 @@ from src.constants import ATTENTION_HEADS
 
 class FFN(nn.Module):
     def __init__(self, in_dim: int, model_dim: int):
+        super().__init__()
         self.lin1 = nn.Linear(in_dim, model_dim)
         self.lin2 = nn.Linear(in_dim, model_dim)
 
@@ -19,6 +20,7 @@ class EncoderLayer(nn.Module):
     def __init__(
         self, in_dim: int, model_dim: int, attn_heads: int = ATTENTION_HEADS
     ):
+        super().__init__()
         self.self_attn = MultiHeadAttention(
             in_dim, in_dim, model_dim, attn_heads
         )
@@ -43,11 +45,12 @@ class DecoderLayer(nn.Module):
         model_dim: int,
         attn_heads: int = ATTENTION_HEADS,
     ):
+        super().__init__()
         self.self_attn = MultiHeadAttention(
             in_dim, in_dim, model_dim, attn_heads
         )
         self.norm1 = nn.LayerNorm(model_dim)
-        self.encoder_attn = MultiHeadAttention(
+        self.cross_attn = MultiHeadAttention(
             encoder_dim, model_dim, model_dim, attn_heads
         )
         self.norm2 = nn.LayerNorm(model_dim)
@@ -55,14 +58,31 @@ class DecoderLayer(nn.Module):
         self.norm3 = nn.LayerNorm(model_dim)
 
     def forward(self, input: torch.Tensor, encoder_output: torch.Tensor):
-        attn_result = self.self_attn(input, input, input)
+        batch_size, seq_len, emb_dim = input.size()
+        self_mask = construct_future_mask(seq_len, batch_size)
+        attn_result = self.self_attn(input, input, input, self_mask)
         attn_result = self.norm1(input + attn_result)
 
-        enc_result = self.encoder_attn(
-            encoder_output, encoder_output, attn_result
+        enc_result = self.cross_attn(
+            attn_result, encoder_output, encoder_output
         )
         enc_result = self.norm2(attn_result + enc_result)
 
         ffn_result = self.ffn(enc_result)
         ffn_result = self.norm2(enc_result + ffn_result)
         return ffn_result
+
+
+# Idea is taken from https://github.com/jsbaan/transformer-from-scratch/blob/main/utils.py
+def construct_future_mask(seq_len: int, batch_size: int = 1):
+    """
+    Construct a binary mask that contains 1's for all previous connections (autoregressive) and 0's for all outgoing future connections.
+    This mask will be applied to the attention logits in decoder self-attention such that all logits with a 0 mask
+    are set to -inf.
+    :param seq_len: length of the input sequence
+    :return: (seq_len,seq_len) mask
+    """
+    subsequent_mask = torch.tril(
+        torch.ones(batch_size, seq_len, seq_len), diagonal=-1
+    )
+    return subsequent_mask
--- src/transformer.py
+++ src/transformer.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from src.constants import N_LAYERS
+from src.constants import DICT_SIZE, N_LAYERS
 from src.embedding import WordEmbedding
 from src.modules import DecoderLayer, EncoderLayer
 
@@ -12,12 +12,15 @@ class Encoder(nn.Module):
         super().__init__()
         self.emb = WordEmbedding(emb_dim)
 
-        layers = [EncoderLayer(emb_dim, model_dim) for _ in range(num_layers)]
-        self.encoder_layers = nn.Sequential(*layers)  # Is it sequential?
+        self.encoder_layers = nn.ModuleList(
+            [EncoderLayer(emb_dim, model_dim) for _ in range(num_layers)]
+        )
 
     def forward(self, input: torch.Tensor):
         input = self.emb(input)
-        return self.encoder_layers(input)
+        for enc in self.encoder_layers:
+            input = enc(input)
+        return input
 
 
 class Decoder(nn.Module):
@@ -31,16 +34,39 @@ class Decoder(nn.Module):
         super().__init__()
         self.emb = WordEmbedding(emb_dim)
 
-        layers = [
-            DecoderLayer(emb_dim, enc_dim, model_dim)
-            for _ in range(num_layers)
-        ]
-        self.decoder_layers = nn.Sequential(*layers)  # Is it sequential?
+        self.decoder_layers = nn.ModuleList(
+            [
+                DecoderLayer(emb_dim, enc_dim, model_dim)
+                for _ in range(num_layers)
+            ]
+        )
 
-        self.final_proj = nn.Linear(model_dim, model_dim)
-
-    def forward(self, input: torch.Tensor):
+    def forward(self, input: torch.Tensor, encoder_output: torch.Tensor):
         input = self.emb(input)
-        input = self.decoder_layers(input)
-        input = self.final_proj(input)
-        return torch.softmax(input, 1)
+
+        for dec in self.decoder_layers:
+            input = dec(input, encoder_output)
+
+        return input
+
+
+class GeneratorTransformer(nn.Module):
+    def __init__(
+        self,
+        emb_dim: int,
+        model_dim: int,
+        num_layers: int = N_LAYERS,
+        dict_size: int = DICT_SIZE,
+    ):
+        super().__init__()
+
+        self.encoder = Encoder(emb_dim, model_dim, num_layers)
+        self.decoder = Decoder(emb_dim, model_dim, model_dim, num_layers)
+
+        self.tokens_proj = nn.Linear(model_dim, dict_size)
+
+    def forward(self, seq_in: torch.Tensor, seq_out: torch.Tensor):
+        enc_out = self.encoder(seq_in)
+        dec_out = self.decoder(seq_out, enc_out)
+        tokens_logits = self.tokens_proj(dec_out)
+        return torch.softmax(tokens_logits, 1)
