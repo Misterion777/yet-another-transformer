8b5ae003b8d43987a77311361e341f911bd82023
Ilya Lasy <ilyalas@gmail.com>
2022-10-28 18:09:18 +0300

    black reformat

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,9 @@ from typing import Literal
 import torch
 from torch.utils.data import Dataset
 
-RETOK = re.compile(r'\w+|[^\w\s]|\n', re.UNICODE)
+RETOK = re.compile(r"\w+|[^\w\s]|\n", re.UNICODE)
+
+
 def re_tokenize(text):
     r"""
     Tokenize using a liberal regular expression.
@@ -15,6 +17,7 @@ def re_tokenize(text):
     """
     return RETOK.findall(text)
 
+
 def split_tokenize(text):
     """
     Tokenize on whitespace and some limited punctuation.
@@ -23,33 +26,36 @@ def split_tokenize(text):
     Use re_tokenize if you want more robust handling of punctuation.
     """
     return (
-        text.replace('.', ' . ')
-        .replace(',', ' , ')
-        .replace(';', ' ; ')
-        .replace(':', ' : ')
-        .replace('!', ' ! ')
-        .replace('?', ' ? ')
+        text.replace(".", " . ")
+        .replace(",", " , ")
+        .replace(";", " ; ")
+        .replace(":", " : ")
+        .replace("!", " ! ")
+        .replace("?", " ? ")
         .split()
     )
+
 
 def space_tokenize(text):
     """
     Tokenize exactly on spaces.
     Useful when text is pre-tokenized.
     """
-    return text.strip().split(' ')
+    return text.strip().split(" ")
+
 
 TOKENIZERS = {
     "re": re_tokenize,
     "space": space_tokenize,
-    "split": split_tokenize
+    "split": split_tokenize,
 }
 
-class Dictionary():
+
+class Dictionary:
     EOS_TOKEN = "<eos>"
     UNK_TOKEN = "<unk>"
 
-    def __init__(self, tokenizer_type : Literal["re","space","split"]="re"):
+    def __init__(self, tokenizer_type: Literal["re", "space", "split"] = "re"):
         self.word2idx = {}
         self.idx2word = []
         self.tokenizer = TOKENIZERS[tokenizer_type]
@@ -64,9 +70,9 @@ class Dictionary():
         return self.word2idx[word]
 
     def add_tokens(self, tokens):
-        return[self.add_token(word) for word in tokens]        
-    
-    def tokenize(self,text):
+        return [self.add_token(word) for word in tokens]
+
+    def tokenize(self, text):
         tokens = self.tokenizer(text)
         return tokens
 
@@ -77,15 +83,16 @@ class Dictionary():
     def __len__(self):
         return len(self.idx2word)
 
+
 # https://www.salesforce.com/products/einstein/ai-research/the-wikitext-dependency-language-modeling-dataset/
 class WikiText(Dataset):
-    def __init__(self, tokens_path:str, dictionary:Dictionary):
+    def __init__(self, tokens_path: str, dictionary: Dictionary):
         self.tokens_path = tokens_path
         self.dictionary = dictionary
-                 
+
         self.tokens = []
         self.token_ids = []
-        with open(self.tokens_path, 'r', encoding="utf8") as f:            
+        with open(self.tokens_path, "r", encoding="utf8") as f:
             for line in f:
                 line_tokens = self.dictionary.tokenize(line)
                 line_ids = self.dictionary.add_tokens(self.tokens)
@@ -95,14 +102,14 @@ class WikiText(Dataset):
     def __len__(self):
         return len(self.token_ids)
 
-    def __getitem__(self, index):            
+    def __getitem__(self, index):
         return torch.tensor(self.token_ids[index])
 
 
 if __name__ == "__main__":
     wiki_dict = Dictionary()
     # train_ds = WikiText("data/wikitext-2/wiki.train.tokens",wiki_dict)
-    val_ds = WikiText("data/wikitext-2/wiki.valid.tokens",wiki_dict)
+    val_ds = WikiText("data/wikitext-2/wiki.valid.tokens", wiki_dict)
     print(len(wiki_dict))
     # print(len(train_ds))
     print(len(val_ds))
--- src/trainer.py
+++ src/trainer.py
@@ -3,25 +3,32 @@ import torch.optim.lr_scheduler as scheduler
 
 from src.dataset import Dictionary, WikiText
 
+
 def get_backend():
     if not torch.backends.mps.is_available():
         if not torch.backends.mps.is_built():
-            print("MPS not available because the current PyTorch install was not "
-                "built with MPS enabled.")
+            print(
+                "MPS not available because the current PyTorch install was not "
+                "built with MPS enabled."
+            )
         else:
-            print("MPS not available because the current MacOS version is not 12.3+ "
-                "and/or you do not have an MPS-enabled device on this machine.")
-    if torch.backends.mps.is_available() and torch.backends.mps.is_built():        
-        return "mps"        
+            print(
+                "MPS not available because the current MacOS version is not 12.3+ "
+                "and/or you do not have an MPS-enabled device on this machine."
+            )
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        return "mps"
     if torch.cuda.is_available():
         return "cuda"
     return "cpu"
 
+
 DEVICE = torch.device(get_backend())
 print(f"Using device: {DEVICE}")
 
+
 class TransformerTrainer:
-    def __init__(self,model):
+    def __init__(self, model):
         self.model = model
 
         self.optimizer = torch.optim.Adam(
@@ -32,8 +39,3 @@ class TransformerTrainer:
         )
         self.criterion = torch.nn.CrossEntropyLoss().to(DEVICE)
         print(self.model)
-
-
-
-    
-    
